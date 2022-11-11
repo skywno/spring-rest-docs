ccaab356a307aa695a135351cda411d45ed5f569
Ezra Lim <skywno@gmail.com>
2022-11-11 13:07:09 +0200

    Implement #2 Custom RestDocs

---
+++ src/test/java/me/ezra/restdocs/TestSupport.java
@@ -0,0 +1,57 @@
+package me.ezra.restdocs;
+
+import io.micrometer.core.instrument.util.IOUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentationConfigurer;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs(outputDir = "build/generated-snippets")
+@Import(RestDocsConfiguration.class)
+public class TestSupport {
+
+    @Autowired
+    protected MockMvc mvc;
+
+    @Autowired
+    protected RestDocumentationResultHandler write;
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    @BeforeEach
+    void setUp(final WebApplicationContext context,
+               @Autowired final RestDocumentationContextProvider provider
+    ) {
+        this.mvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(MockMvcRestDocumentation.documentationConfiguration(provider))
+                .alwaysDo(print())
+                .alwaysDo(write)
+                .build();
+    }
+
+    protected String readJson(final String path) throws IOException {
+        return IOUtils.toString(
+                resourceLoader.getResource("classpath:" + path).getInputStream(),
+                StandardCharsets.UTF_8
+        );
+    }
+}
--- src/test/java/me/ezra/restdocs/member/MemberApiTest.java
+++ src/test/java/me/ezra/restdocs/member/MemberApiTest.java
@@ -2,25 +2,13 @@ package me.ezra.restdocs.member;
 
 import me.ezra.restdocs.TestSupport;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@AutoConfigureRestDocs(outputDir = "build/generated-snippets")
-class MemberApiTest extends TestSupport {
 
-    @Autowired
-    protected MockMvc mvc;
+class MemberApiTest extends TestSupport {
 
     @Test
     public void member_get_all() throws Exception {
@@ -28,19 +16,15 @@ class MemberApiTest extends TestSupport {
                         .param("size", "10")
                         .param("page", "0")
                         .contentType(MediaType.APPLICATION_JSON)
-                ).andDo(print())
-                .andDo(document("{class-name}/{method-name}"))
+                )
                 .andExpect(status().isOk());
     }
 
     @Test
     public void member_get() throws Exception {
         mvc.perform(get("/api/members/{id}", 1L)
-                        .param("size", "10")
-                        .param("page", "0")
                         .contentType(MediaType.APPLICATION_JSON)
-                ).andDo(print())
-                .andDo(document("{class-name}/{method-name}"))
+                )
                 .andExpect(status().isOk());
     }
 
@@ -49,8 +33,7 @@ class MemberApiTest extends TestSupport {
         mvc.perform(post("/api/members")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(readJson("/json/member-api/create.json"))
-                ).andDo(print())
-                .andDo(document("{class-name}/{method-name}"))
+                )
                 .andExpect(status().isOk());
     }
 
@@ -59,8 +42,7 @@ class MemberApiTest extends TestSupport {
         mvc.perform(put("/api/members/{id}", 1L)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(readJson("/json/member-api/update.json"))
-                ).andDo(print())
-                .andDo(document("{class-name}/{method-name}"))
+                )
                 .andExpect(status().isOk());
     }
 
